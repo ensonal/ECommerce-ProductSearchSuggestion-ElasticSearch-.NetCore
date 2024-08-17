241320780b2f830413474345fc8ff9ab2add2eb4
Enes Önal <enes.onal777@gmail.com>
2024-08-17 03:54:14 +0300

    refact: style

--- ecommerce-react/src/components/navbar/NavBar.tsx
+++ ecommerce-react/src/components/navbar/NavBar.tsx
@@ -3,7 +3,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import logo from "../../assets/images/logo.png";
 import { NavCenterActions } from "./NavCenterActions";
 
@@ -12,61 +12,73 @@ export function NavBar() {
     <AppBar
       position="static"
       sx={{ backgroundColor: "white", boxShadow: 1, width: "100%" }}
-      className="shadow-sm px-5"
+      className="shadow-sm"
     >
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Box
+      <Container maxWidth="lg">
+        <Toolbar
           sx={{
             display: "flex",
+            justifyContent: "space-between",
             flexDirection: "row",
             alignItems: "center",
-            justifyContent: "center",
+            justifyItems: "center",
           }}
+          className="p-0"
         >
-          <img src={logo} alt="logo" width={80} height={80} />
-          <Typography variant="h5" color="#303030" sx={{ fontWeight: "600" }}>
-            ShopSphere
-          </Typography>
-        </Box>
-        <NavCenterActions />
-        <Box className="d-flex flex-row gap-2">
           <Box
-            className="d-flex flex-row gap-1 rounded-3 py-1 px-2 shadow-sm"
             sx={{
-              justifyContent: "center",
-              alignItems: "center",
+              display: "flex",
+              width: "20%",
+              justifyContent: "start",
             }}
           >
-            <ShoppingCartOutlinedIcon
-              fontSize="medium"
-              sx={{ color: "#303030" }}
-            />
-            <Typography
-              variant="subtitle1"
-              color="#303030"
-              sx={{ fontWeight: "600" }}
-            >
-              Cart
-            </Typography>
+            <img src={logo} alt="logo" width={80} height={80} />
           </Box>
+          <NavCenterActions />
           <Box
-            className="d-flex flex-row gap-1 rounded-3 py-1 px-2 shadow-sm"
-            sx={{
-              justifyContent: "center",
-              alignItems: "center",
-            }}
+            className="d-flex flex-row gap-3 justify-content-end"
+            sx={{ width: "20%" }}
           >
-            <PermIdentityIcon fontSize="medium" sx={{ color: "#303030" }} />
-            <Typography
-              variant="subtitle1"
-              color="#303030"
-              sx={{ fontWeight: "600" }}
+            <Box
+              className="d-flex flex-row gap-1 rounded-3 py-1 px-2"
+              sx={{
+                justifyContent: "center",
+                alignItems: "center",
+              }}
             >
-              Account
-            </Typography>
+              <ShoppingCartOutlinedIcon
+                fontSize="medium"
+                sx={{ color: "#333333" }}
+              />
+              <Typography
+                variant="subtitle1"
+                color="#333333"
+                fontSize={"0.875rem"}
+                sx={{ fontWeight: "600" }}
+              >
+                Cart
+              </Typography>
+            </Box>
+            <Box
+              className="d-flex flex-row gap-1 rounded-3 py-1 px-2"
+              sx={{
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <PermIdentityIcon fontSize="medium" sx={{ color: "#333333" }} />
+              <Typography
+                variant="subtitle1"
+                color="#333333"
+                fontSize={"0.875rem"}
+                sx={{ fontWeight: "600" }}
+              >
+                Account
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-      </Toolbar>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
--- ecommerce-react/src/components/navbar/NavCenterActions.tsx
+++ ecommerce-react/src/components/navbar/NavCenterActions.tsx
@@ -2,119 +2,45 @@ import { useState } from "react";
 import Input from "@mui/joy/Input";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
-import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import { Box, Typography } from "@mui/material";
 
 export function NavCenterActions() {
-  const [searchActive, setSearchActive] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSearchClick = () => {
-    setSearchActive(true);
-  };
-
-  const handleCloseSearch = () => {
-    setSearchActive(false);
-  };
-
   return (
-    <Box
-      sx={{
-        display: "flex",
-        gap: 3,
-        alignItems: "center",
-        justifyContent: "center",
-        width: "30%",
-      }}
-    >
-      {!searchActive && (
-        <>
-          <Typography
-            variant="subtitle1"
-            color="#303030"
-            sx={{
-              fontWeight: "600",
-            }}
-          >
-            Categories
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            color="#303030"
-            sx={{
-              fontWeight: "600",
-            }}
-          >
-            Deals
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            color="#303030"
-            sx={{
-              fontWeight: "600",
-            }}
-          >
-            What's New
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            color="#303030"
-            sx={{
-              fontWeight: "600",
-            }}
-          >
-            Delivery
-          </Typography>
-          <SearchRoundedIcon
-            color="action"
-            fontSize="small"
-            onClick={handleSearchClick}
-          />
-        </>
-      )}
-      {searchActive && (
-        <div className="d-flex flex-row gap-1 justify-content-center align-items-center w-100">
-          <Input
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            sx={{
-              flex: 1,
-              width: searchActive ? "100%" : "0",
-              "--Input-focusedThickness": "0px",
-            }}
-            placeholder="Search everything at ShopSphere"
-            className="rounded-4"
-            color="neutral"
-            variant="outlined"
-            endDecorator={
-              <div className="d-flex flex-row gap-1">
-                <CloseOutlinedIcon
-                  color="action"
-                  fontSize="small"
-                  onClick={() => setSearchQuery("")}
-                  sx={{
-                    cursor: "pointer",
-                  }}
-                />
-                <SearchRoundedIcon
-                  fontSize="small"
-                  sx={{
-                    cursor: "pointer",
-                  }}
-                />
-              </div>
-            }
-          />
-          <ArrowRightIcon
-            color="action"
-            fontSize="small"
-            onClick={handleCloseSearch}
-            sx={{
-              cursor: "pointer",
-            }}
-          />
-        </div>
-      )}
-    </Box>
+    <div className="d-flex flex-row gap-1 justify-content-center align-items-center w-50">
+      <Input
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        sx={{
+          flex: 1,
+          width: "100%",
+          "--Input-focusedThickness": "0px",
+          fontSize: "0.875rem",
+        }}
+        placeholder="Search everything at ShopSphere"
+        className="rounded-4"
+        color="neutral"
+        variant="outlined"
+        size="lg"
+        endDecorator={
+          <div className="d-flex flex-row gap-1">
+            <CloseOutlinedIcon
+              color="action"
+              fontSize="small"
+              onClick={() => setSearchQuery("")}
+              sx={{
+                cursor: "pointer",
+              }}
+            />
+            <SearchRoundedIcon
+              fontSize="small"
+              sx={{
+                cursor: "pointer",
+              }}
+            />
+          </div>
+        }
+      />
+    </div>
   );
 }
